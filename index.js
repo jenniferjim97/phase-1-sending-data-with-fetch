741d070d8ecf4ea67a5a2adb690a11f1b1faa1e6
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-01-23 17:46:48 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+
+
+function submitData (name, email) {
+  const formData = {
+    "name": name, 
+    "email": email,
+  }
+  
+  const configurationObject = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+        'Accept': 'application/json',
+  },
+  body: JSON.stringify(formData)
+  }
+  return fetch('http://localhost:3000/users', configurationObject)
+ 
+  .then(res => res.json())
+  .then(object => document.body.innerHTML = object['id'])
+  .catch(error => document.body.innerHTML = error.message)
+  }
+
+  submitData("jennifer", "jennifer@email.com");
